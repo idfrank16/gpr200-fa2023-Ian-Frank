e84abb7ddf7777cea057af0f1850c801ee9950c0
Ian <ian.frank@mymail.champlain.edu>
2023-09-05 11:16:32 -0400

    added vertex and fragment shaders

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -15,6 +15,45 @@ int main() {
 		return 1;
 	}
 
+	float vertices[9] = {
+		//x   //y  //z
+		-0.5, -0.5, 0.0, //Bottom left
+		 0.5, -0.5, 0.0, //Bottom right
+		 0.0,  0.5, 0.0  //Top center
+	};
+
+	const char* vertexShaderSource = R"(
+		#version 450
+		layout(location = 0) in vec3 vPos;
+		void main(){
+			gl_Position = vec4(vPos,1.0);
+		}
+	)";
+
+	const char* fragmentShaderSource = R"(
+		#version 450
+		out vec4 FragColor;
+		void main(){
+			FragColor = vec4(1.0);
+		}
+	)";
+
+	//Create a new vertex shader object
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Supply the shader object with source code
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//Compile the shader object
+	glCompileShader(vertexShader);
+
+	int success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message.
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile shader: %s", infoLog);
+	}
+
 	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
 	if (window == NULL) {
 		printf("GLFW failed to create window");
@@ -26,6 +65,22 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
+
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	//Define position attribute (3 floats)
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
